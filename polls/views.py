9ec4acb4182dc14946cf92b63f90c5fe074fca54
Andromeda <Xepypg0@yahoo.com>
2021-03-24 21:52:15 +0300

    Added a template for voting and showing results.

--- polls/views.py
+++ polls/views.py
@@ -15,10 +15,27 @@ def polls(request, name_book):
     question = Question.objects.get(book=book_id)
     context = {'book': book,
                'question': question}
-    return render(request, 'polls/polls.html', context=context)
+    return render(request, 'polls/content_for_voting.html', context=context)
 
 
-def choice(request, name_book, book_id):
+def choice_for_book(request, name_book, book_id):
     question = Question.objects.get(book=book_id)
-    context = {'question': question}
+    book = Book.objects.get(name_book=name_book)
+    context = {'book': book,
+               'question': question}
     return render(request, 'polls/choice.html', context=context)
+
+
+def voting(request, name_book, book_id):
+    question = Question.objects.get(book_id=book_id)
+    answer_options = question.choice_set.get(pk=request.POST['choice'])
+
+    context = {'question': question,
+               'answer_options': answer_options}
+    answer_options.votes += 1
+    answer_options.save()
+    return render(request, 'polls/voting_results.html', context=context)
+
+
+
+
